ad4295efac60523b75233a02925f7d21f1f212c4
Tuti115 <josehuerta115@gmail.com>
2017-05-08 15:51:37 -0700

    work on hw

---
+++ PrincessEve/src/tester.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+
+public class tester
+{
+    public static void main(String[] args)
+    {
+        SuitorList suitor = new SuitorList();
+
+        System.out.println(suitor.findLuckySuitor());
+
+        /*
+        LinkedList<Suitor> list = new LinkedList<>();
+
+        list.add(new Suitor(0));
+        list.add(new Suitor(1));
+        list.add(new Suitor(2));
+        list.add(new Suitor(3));
+        list.add(new Suitor(4));
+
+        System.out.println("Size of LinkedList: " + list.size());
+
+        list.remove(2);
+        list.remove(3);
+
+        System.out.println("Size of LinkedList: " + list.size());
+
+        */
+
+    }
+
+
+}
